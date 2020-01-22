9860c97f5769639e663d61eecba90a772eb6cac3
Kuber <kuberbhu@gmail.com>
2020-01-22 21:00:42 +0530

    visualize data and select featured from pandas df

---
+++ vizualization_pd.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# heatmap of correlation
+f,ax = plt.subplots(figsize=(18, 18))
+sns.heatmap(x.corr(), annot=True, linewidths=.5, fmt= '.1f',ax=ax)
+
