cf2ee24d0de4ad011735dd0ed8f0c46a0e86f738
Kuber <kuberbhu@gmail.com>
2020-01-22 20:04:42 +0530

    Create files_io.py
    reading and writing from csv or csv.gz files as list

---
+++ files_io.py
@@ -0,0 +1,24 @@
+# reading from csv
+# input is file_ = '*.csv'
+# output is list of dict
+inputList = [x for x in csv.DictReader(open(file_,'r'))] 
+
+# reading from csv.gz
+# resultFile = '*.csv.gzip'
+# output is list of dict
+inputTrx = [json.loads(x) for x in gzip.open( resultFile )] 
+
+# writing to csv file
+headerDict = OrderedDict([(x, None) for x in 'col1,col2,col3'.split(',')])
+with open('test_big.csv','wb') as fout:
+    dw = csv.DictWriter(fout, delimiter=',',fieldnames=headerDict, quoting =csv.QUOTE_ALL)
+    dw.writeheader()
+        for item in inputList:
+        dw.writerow(item) # item is dict
+
+# writing to csv.gz file
+with gzip.open(os.path.join(backupPath, "backup.csv.gz"),'w') as outFile:
+    for lJ in inList:
+        row = json.dumps(lJ)
+        outFile.write(row)
+        outFile.write('\n')
