88069f6e16033f84d877229444b0a25f5e2e4751
Kuber <kuberbhu@gmail.com>
2020-01-23 13:07:10 +0530

    added fetching

--- databases.py
+++ databases.py
@@ -5,6 +5,7 @@ cnx = mysql.connector.connect(user='username', password='PASSWORD', host='localh
 cursor = cnx.cursor()
 
 #inserting
+# dict_of_dict contains data we want to load to table
 columns = ['col1','col2',col3'] #list of column names
 start_time = time.time()
 for id_,(k,g) in enumerate(dict_of_dict.items()):
@@ -14,9 +15,12 @@ for id_,(k,g) in enumerate(dict_of_dict.items()):
 		start_time=time.time()
 		cnx.commit()
 	values = ','.join(['"' + str(g.get(f,'')) + '"' for f in columns])
-	query='replace into %s (%s) values (%s);' %('ph_full_kuber',','.join(columns),values)
+	query='replace into %s (%s) values (%s);' %('tablename',','.join(columns),values)
 	cursor.execute(query)
 
 #fetching
+cursor.execute('select col1,col2,col3,col4 from tablename where col1 in ("val1","val2");')
+outList = cursor.fetchall()
+#outList = {val[0]:val for valueList in outList}
 
 
