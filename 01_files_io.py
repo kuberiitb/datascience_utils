face298f9df652635905a48794d9f064d2c55486
Kuber <kuberbhu@gmail.com>
2020-02-21 11:17:30 +0530

    added usage of csv write and writerows

--- 01_files_io.py
+++ 01_files_io.py
@@ -22,3 +22,30 @@ with gzip.open(os.path.join(backupPath, "backup.csv.gz"),'w') as outFile:
         row = json.dumps(lJ)
         outFile.write(row)
         outFile.write('\n')
+
+
+## Write using csv module
+### Write into CSV files with csv.writer()
+import csv
+row_list = [["SN", "Name", "Contribution"],
+             [1, "Linus Torvalds", "Linux Kernel"],
+             [2, "Tim Berners-Lee", "World Wide Web"],
+             [3, "Guido van Rossum", "Python Programming"]]
+with open('innovators.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["SN", "Name", "Contribution"]) # header
+    for row_ in row_list:
+        writer.writerow(row_)  #one line at a time
+
+### Write CSV files with quotes using list of list with csv.writerows()
+import csv
+row_list = [
+    ["SN", "Name", "Quotes"],
+    [1, "Buddha", "What we think we become"],
+    [2, "Mark Twain", "Never regret anything that made you smile"],
+    [3, "Oscar Wilde", "Be yourself everyone else is already taken"]
+]
+with open('quotes.csv', 'w', newline='') as file:
+    writer = csv.writer(file, quoting=csv.QUOTE_NONNUMERIC, delimiter=';')
+    writer.writerows(row_list)
+    
