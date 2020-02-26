5d8aa24fbb54206066d1f6c8de917717550bbff4
Kuber <kuberbhu@gmail.com>
2020-02-26 15:33:52 +0530

    Update pickles.py

--- pickles.py
+++ pickles.py
@@ -1,12 +1,12 @@
 import pickle
-def dump(file_, filename):
-    with open(filename+'.pkl','wb') as f:
+def dump(file_, filename='out.pkl'):
+    with open(filename,'wb') as f:
         pickle.dump(file_, f, pickle.HIGHEST_PROTOCOL)
 
-pickle.dump(file_, open(filename+'.pkl','wb'), pickle.HIGHEST_PROTOCOL)
+pickle.dump(file_, open(filename,'wb'), pickle.HIGHEST_PROTOCOL)
 
 def load(filename):
     with open(filename,'rb') as f:
         return pickle.load(f)
 
-loaded = pickle.load(open(filename.pkl','rb'))
+loaded = pickle.load(open(filename+'.pkl','rb'))
