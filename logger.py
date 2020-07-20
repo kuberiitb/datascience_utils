9fae01469cadb97e3f0e803d99d9235f4f3c49e6
Kuber <kuberbhu@gmail.com>
2020-07-20 10:09:32 +0530

    Create logger.py

---
+++ logger.py
@@ -0,0 +1,11 @@
+#######################################
+# alternate to print
+import logging
+logging.basicConfig(level=logging.INFO)
+
+#instead of using print("TEST"), you can use 
+logging.info("TEST")
+
+#######################################
+
+
