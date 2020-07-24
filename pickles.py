090a12aa669e1d516d625dcca23b027f19a4573c
Kuber <kuberbhu@gmail.com>
2020-07-24 13:54:05 +0530

    pickle: highest protocol to default protocol

--- pickles.py
+++ pickles.py
@@ -1,9 +1,9 @@
 import pickle
 def dump(file_, filename='out.pkl'):
     with open(filename,'wb') as f:
-        pickle.dump(file_, f, pickle.HIGHEST_PROTOCOL)
+        pickle.dump(file_, f, pickle.DEFAULT_PROTOCOL)
 
-pickle.dump(file_, open(filename,'wb'), pickle.HIGHEST_PROTOCOL)
+pickle.dump(file_, open(filename,'wb'), pickle.DEFAULT_PROTOCOL)
 
 def load(filename):
     with open(filename,'rb') as f:
